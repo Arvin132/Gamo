4a84064658b341e892f41d9936ee385402cf092b
Arvin132 <asgharianarvin@gmail.com>
2024-10-16 05:07:11 -0400

    cleaned up the code, finished issue

--- src/be/unit_test.py
+++ src/be/unit_test.py
@@ -2,26 +2,26 @@ import unittest
 from backend import app
 import json
 
-class GamoFlaskTest_Backend(unittest.TestCase):
+class GamoFlaskTest_Backend_Connect4(unittest.TestCase):
 
     # Setup method to configure the test environment
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-        # When using test_client, the port is irrelevant because requests are simulated internally
 
     def test_running(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertFalse(response.data.decode() == None)
+        self.assertTrue(response.data.decode() != None) # check for any response
         
-    def test_connect4_agents_list(self):
+    def test_agents_list(self):
         response = self.app.get('/get-agents-list')
         data_dict = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(data_dict["agents"])
+        self.assertIsNotNone(data_dict.get("agents")) # check that agents exist
         
-    def test_connect4_start_connect4(self):
+    def test_start_game(self):
+        # check for all the different possible assignments
         response = self.app.post('/start-game-connect4',\
                                     json={'player-1': "Humen", 'player-2': "Humen"})
         data_dict = json.loads(response.data.decode('utf-8'))
@@ -42,7 +42,7 @@ class GamoFlaskTest_Backend(unittest.TestCase):
         
         
         
-    def test_get_state_connect4(self):
+    def test_get_state(self):
         self.app.post('/start-game-connect4',\
                                     json={'player-1': "Humen", 'player-2': "Humen"})
         response = self.app.get('/get-state-connect4')
@@ -54,27 +54,27 @@ class GamoFlaskTest_Backend(unittest.TestCase):
         self.assertIsInstance(data_dict["moves"], list)
         
         
-    def test_apply_move_connect(self):
+    def test_apply_move(self):
+        # check that move from Humen side gets applied
         self.app.post('/start-game-connect4',\
                                     json={'player-1': "Humen", 'player-2': "Humen"})
         response = self.app.post('/apply-move', json={"column": 1, "player-id": 1, "is-bot": False})
         data_dict = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue(data_dict["message"].startswith("200"))
-        
         response = self.app.get('/get-state-connect4')
         data_dict = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue(data_dict["message"].startswith("200"))
         self.assertEqual(data_dict["state"]["board"][-1][0], 1.0)
-
+        
+        # check that move from bots gets applied
         self.app.post('/start-game-connect4',\
                                     json={'player-1': "Random Agent", 'player-2': "Humen"})
         response = self.app.post('/apply-move', json={"column": 1, "player-id": 1, "is-bot": True})
         data_dict = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue(data_dict["message"].startswith("200"))
-        
         response = self.app.get('/get-state-connect4')
         data_dict = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
